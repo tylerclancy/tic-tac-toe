c4472ef1f41a9d93602307839ececb582ab67888
Tyler <tylerclancy030@gmail.com>
2022-01-14 19:31:05 -0600

    Add class name to Board component

--- game.jsx
+++ game.jsx
@@ -1,10 +1,13 @@
 const Board = () => {
   return (
-    <div id="info">
-      <h1>h1</h1>
+    <div className="game-board">
+      <div id="info">
+        <h1>h1</h1>
+      </div>
     </div>
   );
 };
+
 
 // =========================================
 ReactDOM.render(<Board />, document.getElementById('root'));
