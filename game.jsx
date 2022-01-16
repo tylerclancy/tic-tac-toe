1a5c05ebf7339dc895c456d1e1c96393969559a9
Tyler <tylerclancy030@gmail.com>
2022-01-15 23:06:04 -0600

    Create square component and begin grid layout

--- game.jsx
+++ game.jsx
@@ -1,12 +1,18 @@
+const Square = () => {
+  return (
+    <button></button>
+  );
+};
+
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`
-
+  function renderSquare(i) {
+    return <Square />
+  }
   return (
-    <div className="game-board" onClick={e => {
-      setPlayer((player + 1) % 2);
-      status = `Player ${player}`
-    }}>
+    <div className="game-board">
+    <div className="grid-row">{renderSquare(1)}</div>
       <div id="info">
         <h1>{status}</h1>
       </div>
